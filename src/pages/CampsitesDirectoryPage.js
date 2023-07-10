a3bb32abd54b41649007410af73cd89af129bed5
Alicia Pearse <web.alipea@gmail.com>
2023-07-09 22:13:29 -0700

    Breadcrumbs SubHeader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,12 +1,16 @@
-import { Container } from 'reactstrap';
-import CampsitesList from '../features/campsites/CampsitesList';
+import { Container, Row } from "reactstrap";
+import CampsitesList from "../features/campsites/CampsitesList";
+import SubHeader from "../components/SubHeader";
 
 const CampsitesDirectoryPage = () => {
-    return (
-        <Container>
-            <CampsitesList />
-        </Container>
-    );
+  return (
+    <Container>
+      <SubHeader current='Directory' />
+      <Row>
+        <CampsitesList />
+      </Row>
+    </Container>
+  );
 };
 
 export default CampsitesDirectoryPage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,16 @@
-import { Container } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 import DisplayList from '../features/display/DisplayList';
+import SubHeader from '../components/SubHeader';
+
 
 const HomePage = () => {
     return (
         <Container>
+        <SubHeader current='Home' />
+        <Row>
             <DisplayList />
+
+        </Row>
         </Container>
     );
 };
